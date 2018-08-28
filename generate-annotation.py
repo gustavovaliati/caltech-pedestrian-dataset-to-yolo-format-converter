771ac3ee30f54f1f968069870301401da3be2f63
Gustavo Valiati <gustavovaliati@gmail.com>
2018-08-28 20:40:57 -0300

    Modified to match experimentation needs and debugging

--- generate-annotation.py
+++ generate-annotation.py
@@ -2,15 +2,18 @@
 # - https://github.com/mitmul/caltech-pedestrian-dataset-converter/blob/master/scripts/convert_annotations.py
 # - https://pjreddie.com/media/files/voc_label.py
 
-import os
+import os, sys
 import glob
 from scipy.io import loadmat
+from pathlib import Path
 
 
 classes = ['person', 'people'] # others are 'person-fa' and 'person?'
-squared = True
+squared = False
 frame_size = (640, 640) if squared else (640, 480)
 number_of_truth_boxes = 0
+number_of_images = 0
+number_of_images_without_objs = 0
 datasets = {
 	'train' : open('train' + ('_squared' if squared else '')  + '.txt', 'w'),
 	'test' : open('test' + ('_squared' if squared else '')  + '.txt', 'w')
@@ -30,13 +33,17 @@ def convertBoxFormat(box):
 	return (x, y, w, h)
 
 # traverse sets
-for caltech_set in sorted(glob.glob('../caltech/annotations/set*')):
-	set_nr = os.path.basename(caltech_set).replace('set', '')
-	dataset = 'train' if int(set_nr) < 6 else 'test'
-	set_id = dataset + set_nr
+for caltech_set in sorted(glob.glob('/media/gustavo/GRV/datasets/CaltechPedestrians/original/annotations/set*')):
+	# set_nr = os.path.basename(caltech_set).replace('set', '')
+	# dataset = 'train' if int(set_nr) < 6 else 'test'
+	# set_id = dataset + set_nr
+
+	set_id = os.path.basename(caltech_set)
+	dataset = 'train' if int(set_id[3:5]) < 6 else 'test'
 
 	# traverse videos
 	for caltech_annotation in sorted(glob.glob(caltech_set + '/*.vbb')):
+		print(caltech_annotation)
 		vbb = loadmat(caltech_annotation)
 		obj_lists = vbb['A'][0][0][1][0]
 		obj_lbl = [str(v[0]) for v in vbb['A'][0][0][4][0]]
@@ -44,15 +51,23 @@ for caltech_set in sorted(glob.glob('../caltech/annotations/set*')):
 
 		# traverse frames
 		for frame_id, obj in enumerate(obj_lists):
-			if len(obj) > 0:
+			if dataset == 'train':
+				if (frame_id + 1) % 3 != 0:
+					#Skip this frame. Each 3 frames, uses only the 3rd.
+					continue
+			elif (frame_id + 1) % 30 != 0: # dataset == 'test'
+				#Skip this frame. Each 30 frames, uses only the 30th.
+				continue
 
+			number_of_images +=1
+			if len(obj) > 0:
 				# traverse labels
 				labels = ''
 				for pedestrian_id, pedestrian_pos in zip(obj['id'][0], obj['pos'][0]):
 					pedestrian_id = int(pedestrian_id[0][0]) - 1
 					pedestrian_pos = pedestrian_pos[0].tolist()
 					# class filter and height filter: here example for medium distance
-					if obj_lbl[pedestrian_id] in classes and pedestrian_pos[3] > 30 and pedestrian_pos[3] <= 80:
+					if obj_lbl[pedestrian_id] in classes:# and pedestrian_pos[3] > 30 and pedestrian_pos[3] <= 80:
 						class_index = classes.index(obj_lbl[pedestrian_id])
 						yolo_box_format = convertBoxFormat(pedestrian_pos)
 						labels += str(class_index) + ' ' + ' '.join([str(n) for n in yolo_box_format]) + '\n'
@@ -63,13 +78,21 @@ for caltech_set in sorted(glob.glob('../caltech/annotations/set*')):
 					continue
 
 				image_id = set_id + '_' + video_id + '_' + str(frame_id)
-				datasets[dataset].write(os.getcwd() + '/images/' + image_id + ('_squared' if squared else '') + '.png\n')
+				datasets[dataset].write(os.getcwd() + '/images/' + image_id + ('_squared' if squared else '') + '.jpg\n')
 				label_file = open('labels/' + image_id + ('_squared' if squared else '') + '.txt', 'w')
 				label_file.write(labels)
 				label_file.close()
 
-				print('finished ' + image_id)
+				# print('finished ' + image_id)
+			else:
+				image_id = set_id + '_' + video_id + '_' + str(frame_id)
+				datasets[dataset].write(os.getcwd() + '/images/' + image_id + ('_squared' if squared else '') + '.jpg\n')
+				Path('labels/' + image_id + ('_squared' if squared else '') + '.txt').touch()
+				number_of_images_without_objs += 1
+
 
 for dataset in datasets.values():
 	dataset.close()
-print(number_of_truth_boxes) # useful for statistics
+print('number_of_truth_boxes',number_of_truth_boxes) # useful for statistics
+print('number_of_images',number_of_images)
+print('number_of_images_without_objs',number_of_images_without_objs)
--- generate-images.py
+++ generate-images.py
@@ -6,7 +6,7 @@ import cv2 as cv
 
 
 def save_img(dname, fn, i, frame):
-	cv.imwrite('{}/{}_{}_{}.png'.format(
+	cv.imwrite('{}/{}_{}_{}.jpg'.format(
 		out_dir, os.path.basename(dname),
 		os.path.basename(fn).split('.')[0], i), frame)
 
@@ -16,6 +16,7 @@ if not os.path.exists(out_dir):
 
 def convert(dir):
 	for dname in sorted(glob.glob(dir)):
+		print(dname)
 		for fn in sorted(glob.glob('{}/*.seq'.format(dname))):
 			cap = cv.VideoCapture(fn)
 			i = 0
@@ -27,5 +28,27 @@ def convert(dir):
 				i += 1
 			print(fn)
 
-convert('../caltech/train*')
-convert('../caltech/test*')
+
+def convert_to_caltech10x(dir, skipper=30):
+	'''
+	Training set: Caltech 10x.
+	Uses only every 3rd frame.
+	1. Brazil, G., Yin, X., & Liu, X. (2017). Illuminating Pedestrians via Simultaneous Detection & Segmentation. Retrieved from http://arxiv.org/abs/1706.08564
+	2. Zhang, S., Benenson, R., & Schiele, B. (n.d.). Filtered Channel Features for Pedestrian Detection. Retrieved from https://arxiv.org/pdf/1501.05759.pdf
+	'''
+	for dname in sorted(glob.glob(dir)):
+		print(dname)
+		for fn in sorted(glob.glob('{}/*.seq'.format(dname))):
+			cap = cv.VideoCapture(fn)
+			i = 1
+			while True:
+				ret, frame = cap.read()
+				if not ret:
+					break
+				if (i % skipper) == 0:
+					save_img(dname, fn, i-1, frame)
+				i += 1
+			print(fn)
+
+# convert_to_caltech10x('/media/gustavo/GRV/datasets/CaltechPedestrians/original/videos/train/*', skipper=3)
+convert_to_caltech10x('/media/gustavo/GRV/datasets/CaltechPedestrians/original/videos/test/*', skipper=30)
